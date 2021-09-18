9ad18643fdc65b7f14281383507af8ef739e12df
l33th <l33th@protonmail.ch>
2021-09-18 11:48:14 +0200

    With Navigation Bar

--- src/App.js
+++ src/App.js
@@ -1,39 +1,39 @@
 import React, { useState } from "react";
-import Header from "./Header";
 import Menu from "./Menu";
 import Categories from "./Categories";
 import items from "./data";
+import Navbar from "./Navbar";
 
 const allCategories = ["all", ...new Set(items.map((item) => item.category))];
 console.log(allCategories);
 
 function App() {
-  const [menuItems, setMenuItems] = useState(items);
-  const [categories] = useState(allCategories);
-  // const [categories, allCategories] = useState(allCategories);
+  
+	const [menuItems, setMenuItems] = useState(items);
+	const [categories] = useState(allCategories);
 
-  const filterItems = (category) => {
-    if (category === "all") {
-      setMenuItems(items);
-      return;
-    }
-    const newItems = items.filter((item) => item.category === category);
-    setMenuItems(newItems);
-  };
+	const filterItems = (category) => {
+		if (category === "all") {
+			setMenuItems(items);
+			return;
+		}
+		const newItems = items.filter((item) => item.category === category);
+		setMenuItems(newItems);
+	};
 
-  return (
-      <section className="menu section">
-        <div className="header">
-          <Header />
-        </div>
-        <div className="title">
-          {/* <h2>Vegan Meals</h2> */}
-          <div className="underline"></div>
-        </div>
-        <Categories categories={categories} filterItems={filterItems} />
-        <Menu items={menuItems} />
-      </section>
-  );
+	return (
+		<section className='menu section'>
+				<Navbar /> <br />
+			<div className='title'>
+				<div className='underline'></div>
+				<h5 className='wm'>by WMouton</h5>
+			</div>
+			<div>
+			</div>
+			<Categories categories={categories} filterItems={filterItems} />
+			<Menu items={menuItems} />
+		</section>
+	);
 }
 
 export default App;
