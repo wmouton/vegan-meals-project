cb2e3162329c4adcb34a46d63ada6d2ade1bbd21
l33th <l33th@protonmail.ch>
2021-09-18 14:27:37 +0200

    Minor Fixes

---
+++ build/precache-manifest.686db68294d9e4d14f1eaf5f768a5052.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44fa1245855410525cac3afb9e671fe3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "024dc51a0c40cf86dd5f",
+    "url": "/static/css/main.32d507f3.chunk.css"
+  },
+  {
+    "revision": "bd4037dc40cab105e6da",
+    "url": "/static/js/2.0dd6028c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0dd6028c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "024dc51a0c40cf86dd5f",
+    "url": "/static/js/main.d7d237ee.chunk.js"
+  },
+  {
+    "revision": "bb4930c79a0f9f42de61",
+    "url": "/static/js/runtime-main.cf3d6cfd.js"
+  },
+  {
+    "revision": "1b8badf20fd4a4d115345fb60d304e5f",
+    "url": "/static/media/logo.1b8badf2.png"
+  }
+]);
